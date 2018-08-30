2b2c6d9a01ca3adf856a3ed2b9a2e74701c2c0b5
Jerick Bilalat <jrk.mb.xm@gmail.com>
2018-08-30 12:50:48 -0500

    build basic server

---
+++ server.js
@@ -0,0 +1,13 @@
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req,res) => {
+    res.send('Hello World');
+});
+
+app.set('port', process.env.PORT || 3000);
+
+const server = app.listen(app.get('port'),() => {
+    console.log('Express listening to port: ' +  server.address().port);
+});
