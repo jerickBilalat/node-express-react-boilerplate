8441e4a060f6a79db70fbf3f44b472cb1b1a210f
Jerick Bilalat <jrk.mb.xm@gmail.com>
2018-09-09 08:56:23 -0500

    remove controller directory index.js file

--- authetication/controllers/login.js
+++ authetication/controllers/login.js
@@ -1,5 +1,5 @@
 
-
+// note: need the what is the user role to know which components they have access to
 
 module.exports.login = (req,res, next) => {
     console.log('login');
--- authetication/routes.js
+++ authetication/routes.js
@@ -1,10 +1,12 @@
 
-const AuthControllers = require('./controllers');
 const requireMember = require('./middlewares/requireMember');
+
+const {signup: signupController} = require('./controllers/signup');
+const {login: loginController} = require('./controllers/login');
 
 module.exports = app => {
 
-    app.post('/signup', requireMember, AuthControllers.signup);
-    app.post('/login', AuthControllers.login)
+    app.post('/signup', requireMember, signupController);
+    app.post('/login', loginController);
 
 }
