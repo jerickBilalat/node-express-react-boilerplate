411828987084f6501a175b431e2eceb4a7d45b36
Jerick Bilalat <jrk.mb.xm@gmail.com>
2018-09-15 13:51:53 -0500

    make intial singin route and controller

--- authetication/controllers/signin.js
+++ authetication/controllers/signin.js
@@ -1,6 +1,6 @@
 
 // note: need the what is the user role to know which components they have access to
 
-module.exports.login = (req,res, next) => {
-    console.log('login');
+module.exports.signin = (req,res, next) => {
+    res.send({message: "signin"});
 }
--- authetication/routes.js
+++ authetication/routes.js
@@ -2,11 +2,11 @@
 const requireMember = require('./middlewares/requireMember');
 
 const {signup: signupController} = require('./controllers/signup');
-const {login: loginController} = require('./controllers/login');
+const {signin: signinController} = require('./controllers/signin');
 
 module.exports = app => {
 
     app.post('/signup', requireMember, signupController);
-    app.post('/login', loginController);
+    app.post('/signin', signinController);
 
 }
